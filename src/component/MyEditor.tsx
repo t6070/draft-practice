a91e2c5dc73dd4cbe60172cfd2fe4530556135c5
t6070 <taiseigoto325@gmail.com>
2022-03-09 03:25:19 +0900

    add comment

--- src/component/MyEditor.tsx
+++ src/component/MyEditor.tsx
@@ -1,12 +1,34 @@
 import React, { useState } from "react";
 import "draft-js/dist/Draft.css";
 import "../App.css";
-const { Editor, EditorState } = require("draft-js");
+import { convertFromRaw, Editor, EditorState } from "draft-js";
+
+//initDataとeditorStateの形は同じ
+const initData = convertFromRaw({
+  //テキスト装飾のためのメタ情報を格納したオブジェクト
+  entityMap: {},
+  blocks: [
+    {
+      key: "xxxxxx", // ユニークなキー値
+      text: "ここに初期テキストがはいります。", // 任意のテキスト
+      type: "unstyled", // テキストのタイプ。初期値は "unstyled"
+      depth: 0,
+      entityRanges: [],
+      inlineStyleRanges: [],
+      data: {},
+    },
+  ],
+});
+
+const initState = EditorState.createWithContent(initData);
 
 const MyEditor = () => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
+  //空に設定する場合は「EditorState.createEmpty()」
+  //   const [editorState, setEditorState] = useState(() =>
+  //     EditorState.createEmpty()
+  //   );
+
+  const [editorState, setEditorState] = useState(initState);
 
   return (
     <div className="wrapper">
